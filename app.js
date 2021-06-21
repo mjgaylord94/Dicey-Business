7844d8d76b039c36747f2143cf5650069d462fe8
Mitchell <Mitchell@Family-Mac-mini-2.local>
2021-06-21 15:47:52 -0500

    Dice Faces Implemented.

--- app.js
+++ app.js
@@ -11,15 +11,33 @@ class Die {
         this.value = this.roll()
 
         this.div.addEventListener(`click`, () => this.roll())
-        this.div.addEventListener(`dblclick`, () => this.div.remove())
         rollButton.addEventListener(`click`, () => this.roll())
+        // rolling all could also be achieved by pushing each class when constructed into a global array and looping through on click to run roll()
+        this.div.addEventListener(`dblclick`, () => this.div.remove())
+        // if you made a global array, you would need to remove the die from the array as well
     }
 
     roll() {
         let random = Math.floor(Math.random() * 6) + 1
         this.value = random
+        this.getChar()
+        this.div.innerText = this.char
+    }
 
-        this.div.innerText = this.value
+    getChar() {
+        if (this.value === 1) {
+            this.char = "⚀"
+        } else if (this.value === 2) {
+            this.char = "⚁"
+        } else if (this.value === 3) {
+            this.char = "⚂"
+        } else if (this.value === 4) {
+            this.char = "⚃"
+        } else if (this.value === 5) {
+            this.char = "⚄"
+        } else if (this.value === 6) {
+            this.char = "⚅"
+        }
     }
 }
 
@@ -32,8 +50,22 @@ totalButton.addEventListener(`click`, function() {
     let total = 0
 
     for (i = 0; i < dice.length; i++) {
-        total += parseInt(dice[i].innerText)
+        if (dice[i].innerText === "⚀") {
+            total += 1
+        } else if (dice[i].innerText === "⚁") {
+            total += 2
+        } else if (dice[i].innerText === "⚂") {
+            total += 3
+        } else if (dice[i].innerText === "⚃") {
+            total += 4
+        } else if (dice[i].innerText === "⚄") {
+            total += 5
+        } else if (dice[i].innerText === "⚅") {
+            total += 6
+        }
     }
 
     alert(`${total}`)
+    // if using numbers instead of die faces, you would need to convert innertext to intergers using parseInt
+    // if wanting to use this.value to achieve the sum, you would need to have pushed dice into a global array upon creation and loop through to add the value to the total
 })
